584d48805c5fd07f68d3185260929ccc7bac7ac6
0xPh0enix <51119313+0xPh0enix@users.noreply.github.com>
2019-11-03 11:20:15 +0300

    Add files via upload

---
+++ Patcher/Patcher.cpp
@@ -0,0 +1,29 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+int main()
+{
+	SetConsoleTitleA("Simple C++ Binary Patch by 0xPh0enix");
+
+	HANDLE hFile = CreateFileA("Test.exe", GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	DWORD dwWritten = 0;
+	DWORD dwOffset = 0x130D;
+
+	if (hFile == INVALID_HANDLE_VALUE) {
+		MessageBoxA(NULL, "Failed patch file: " + GetLastError(), "", MB_OK);
+	}
+
+	char patch[] = { 'A' };
+
+	//0x00006135
+
+	SetFilePointer(hFile, dwOffset, 0, FILE_BEGIN);
+	WriteFile( hFile, patch, sizeof(patch), &dwWritten, NULL);
+	CloseHandle(hFile);
+
+
+
+
+
+}
---
+++ Test/Test.cpp
@@ -0,0 +1,11 @@
+﻿#include <Windows.h>
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+	MessageBoxA(NULL, "HUCK", "", MB_OK);
+	ExitProcess(0);
+}
